d2b59422617f9f5c80c322917f734d3834a7882b
- <walden4u@gmail.com>
2022-06-07 06:25:15 +0000

    number_operator

--- number.php
+++ number.php
@@ -1,4 +1,11 @@
-<?php
-	echo 1;
-	print(1);
-?>
+<!DOCTYPE html>
+<html>
+	<body>
+		<h1>Number & Arithmetic Operator</h1>
+		<h2> 3 / 2 = </h2>
+		<?php 
+			echo 3 / 2;
+		?>
+		
+	</body>
+</html>
